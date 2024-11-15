329c740b2b69b7279d55ed3cb91c41218f374b93
sachinsainni <sachins98169@gmail.com>
2024-11-16 02:24:41 +0530

    add /api

--- src/main/java/com/sachin/controller/UserController.java
+++ src/main/java/com/sachin/controller/UserController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +28,7 @@ import com.sachin.service.CustomUserDetailsService;
 
 import jakarta.validation.Valid;
 
+@RequestMapping("/api")
 @CrossOrigin(origins = "*")
 @RestController
 @Validated
--- src/main/java/com/sachin/socket/WebSocketConfig.java
+++ src/main/java/com/sachin/socket/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-		registry.addHandler(new MessageHandler(), "/chat").setAllowedOrigins("*")
+		registry.addHandler(new MessageHandler(), "/api/chat").setAllowedOrigins("*")
 				.addInterceptors(jwtHandshakeInterceptor);
 
 	}
